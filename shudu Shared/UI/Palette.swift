c722ad959481de18e2b252c692ba365c803a97f3
Sylar <swlfigo@gmail.com>
2026-09-17 00:25:58 +0800

    feat: draw sudoku board cells in SpriteKit

---
+++ shudu Shared/UI/Palette.swift
@@ -0,0 +1,18 @@
+import SpriteKit
+
+enum Palette {
+    static let background = SKColor(red: 0x1C / 255.0, green: 0x19 / 255.0, blue: 0x16 / 255.0, alpha: 1)
+    static let paper = SKColor(red: 0xEF / 255.0, green: 0xE4 / 255.0, blue: 0xCC / 255.0, alpha: 1)
+    static let ink = SKColor(red: 0x2B / 255.0, green: 0x21 / 255.0, blue: 0x18 / 255.0, alpha: 1)
+    static let userInk = SKColor(red: 0x3D / 255.0, green: 0x5C / 255.0, blue: 0x8A / 255.0, alpha: 1)
+    static let selected = SKColor(red: 0xD7 / 255.0, green: 0xC4 / 255.0, blue: 0x9A / 255.0, alpha: 1)
+    static let peer = SKColor(red: 0xF4 / 255.0, green: 0xEA / 255.0, blue: 0xD4 / 255.0, alpha: 1)
+    static let sameDigit = SKColor(red: 0xE4 / 255.0, green: 0xD4 / 255.0, blue: 0xA8 / 255.0, alpha: 1)
+    static let conflictFill = SKColor(red: 0xF0 / 255.0, green: 0xC8 / 255.0, blue: 0xB4 / 255.0, alpha: 1)
+    static let conflictInk = SKColor(red: 0x8A / 255.0, green: 0x2A / 255.0, blue: 0x1A / 255.0, alpha: 1)
+    static let note = SKColor(red: 0x7A / 255.0, green: 0x6A / 255.0, blue: 0x58 / 255.0, alpha: 1)
+    static let key = SKColor(red: 0x2A / 255.0, green: 0x24 / 255.0, blue: 0x20 / 255.0, alpha: 1)
+    static let hintKey = SKColor(red: 0x3A / 255.0, green: 0x4A / 255.0, blue: 0x38 / 255.0, alpha: 1)
+    static let noteOn = SKColor(red: 0xD4 / 255.0, green: 0xB4 / 255.0, blue: 0x83 / 255.0, alpha: 1)
+    static let line = SKColor(red: 0x3A / 255.0, green: 0x2F / 255.0, blue: 0x24 / 255.0, alpha: 1)
+}
