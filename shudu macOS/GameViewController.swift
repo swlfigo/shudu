8bfc81b0176d5a5423581ac6fc9b63fda16fdf1a
Sylar <swlfigo@gmail.com>
2026-09-16 22:28:29 +0800

    Initial Commit

---
+++ shudu macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  shudu macOS
+//
+//  Created by sylar on 2026/9/16.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
